713b0d59dd8a9d5beae74b67d84b4b8bfc600493
nandaa-rizz <nandaaskyy@gmail.com>
2025-02-05 13:40:35 +0700

    menambahkan card layanan

--- app/src/main/java/com/nanda/laundry/layanan/DataLayananActivity.kt
+++ app/src/main/java/com/nanda/laundry/layanan/DataLayananActivity.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.nanda.laundry.R
 
-class LayananCuci : AppCompatActivity() {
+class DataLayananActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_layanan_cuci)
+        setContentView(R.layout.activity_data_layanan)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
